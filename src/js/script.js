5092ced4f18f601e58a9482e97226152224cacf9
Alexandr <Pech228@list.ru>
2021-04-03 19:52:56 +0300

    first

---
+++ src/js/script.js
@@ -0,0 +1,58 @@
+var name = 'ivav';
+
+let number = 3;
+const pi = 3.14;
+
+number = 4;
+
+let left = 200;
+
+// number
+// string - ''
+// true/false
+// null 
+// undefined
+/* let obj = {
+    name: 'apple',
+    color: 'green',
+    weigth: 200
+} */
+
+/* let isChacked = true,
+    isFalse = false;
+
+console.log(isChacked || isFalse) */;
+
+/* if (2*4 == 8*1) {
+    console.log("good")
+}else {
+    console.log("error")
+} */
+
+/* let answer = confirm("Есть 18?")
+if (answer) {
+    console.log("ok")
+}else {
+    console.log('gg')
+} */
+
+/* const num = 50
+
+if (num < 49) {
+    console.log("a?")
+}else if (num > 100) {
+    console.log('no')
+}else {
+    console.log('yes')
+} */
+
+
+/* for (let i = 1; i < 8; i++) {
+    console.log(i);
+}
+
+function log(a, b) {
+    console.log(a + b)
+}
+
+log(4, 5); */
