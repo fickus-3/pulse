3e17a4f3b7670a0d39d771804ac8004e0c836abe
Alexandr <Pech228@list.ru>
2021-04-28 18:02:40 +0300

    second commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,58 +1,17 @@
-var name = 'ivav';
-
-let number = 3;
-const pi = 3.14;
-
-number = 4;
-
-let left = 200;
-
-// number
-// string - ''
-// true/false
-// null 
-// undefined
-/* let obj = {
-    name: 'apple',
-    color: 'green',
-    weigth: 200
-} */
-
-/* let isChacked = true,
-    isFalse = false;
-
-console.log(isChacked || isFalse) */;
-
-/* if (2*4 == 8*1) {
-    console.log("good")
-}else {
-    console.log("error")
-} */
-
-/* let answer = confirm("Есть 18?")
-if (answer) {
-    console.log("ok")
-}else {
-    console.log('gg')
-} */
-
-/* const num = 50
-
-if (num < 49) {
-    console.log("a?")
-}else if (num > 100) {
-    console.log('no')
-}else {
-    console.log('yes')
-} */
-
-
-/* for (let i = 1; i < 8; i++) {
-    console.log(i);
-}
-
-function log(a, b) {
-    console.log(a + b)
-}
-
-log(4, 5); */
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 300,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"> <img src = "../img/icons/arrow_left.png"> </button>',
+        nextArrow: '<button type="button" class="slick-next"> <img src = "../img/icons/arrow_right.png"> </button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+      });
+  });
